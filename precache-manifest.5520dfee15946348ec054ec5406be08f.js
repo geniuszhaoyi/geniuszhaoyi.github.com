51590ca413edcae336fdae5eac709e7c45234b74
Yi Zhao <genius.zhaoyi@gmail.com>
2020-05-30 01:27:29 -0400

    new page

---
+++ precache-manifest.5520dfee15946348ec054ec5406be08f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7b9f3bdd2d92e0e22d8691b4d078c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ebc9bbd494ef6ec52ff",
+    "url": "/static/css/main.636f0dda.chunk.css"
+  },
+  {
+    "revision": "7b50488feda6ccd6d181",
+    "url": "/static/js/2.2e23a62c.chunk.js"
+  },
+  {
+    "revision": "4755fe18a4b99e0930c01385178d126f",
+    "url": "/static/js/2.2e23a62c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ebc9bbd494ef6ec52ff",
+    "url": "/static/js/main.9a7cbbc7.chunk.js"
+  },
+  {
+    "revision": "80e274ec0ec388e5e202",
+    "url": "/static/js/runtime-main.c8b434d9.js"
+  }
+]);
