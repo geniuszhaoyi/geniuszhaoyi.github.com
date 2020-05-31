0f1e7b848df74c7c739630250996eabf46b4a396
Yi Zhao <genius.zhaoyi@gmail.com>
2020-05-30 22:03:48 -0400

    add colorful resume

---
+++ precache-manifest.4a7556a50bb3f2ef8a4fc508159f9a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46272ac79b6f2af3ce95773d07ffe814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a802dedffc4e68d5aa9",
+    "url": "/static/css/main.779ac46c.chunk.css"
+  },
+  {
+    "revision": "518f1426a9ed6b113d22",
+    "url": "/static/js/2.83ef33e3.chunk.js"
+  },
+  {
+    "revision": "4755fe18a4b99e0930c01385178d126f",
+    "url": "/static/js/2.83ef33e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a802dedffc4e68d5aa9",
+    "url": "/static/js/main.fade0276.chunk.js"
+  },
+  {
+    "revision": "80e274ec0ec388e5e202",
+    "url": "/static/js/runtime-main.c8b434d9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7523053bed41da171750510b6e01b2c5.js"
+  "/precache-manifest.4a7556a50bb3f2ef8a4fc508159f9a12.js"
 );
 
 self.addEventListener('message', (event) => {
